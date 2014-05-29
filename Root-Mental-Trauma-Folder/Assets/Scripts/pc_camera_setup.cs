d427e73c9ae34fa66324f101828c962178bc6d39
Benjamin gordon <bgordon@bppwd.com>
2014-05-28 22:12:50 -0400

    Imported Project Workspace

---
+++ Root-Mental-Trauma-Folder/Assets/Scripts/pc_camera_setup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pc_camera_setup : MonoBehaviour {
+
+	public float height;
+	public float tilt;
+	public float viewing_angle;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform.position.x
+}
