09d80373e95c3e666d1c3e8793c183b674ceecfb
Ben Gordon <bgordon@bppwd.com>
2014-05-29 13:36:11 -0400

    Added debugBox

--- Root-Mental-Trauma-Folder/Assets/Scripts/pc_camera_setup.cs
+++ Root-Mental-Trauma-Folder/Assets/Scripts/pc_camera_setup.cs
@@ -2,16 +2,16 @@
 using System.Collections;
 
 public class pc_camera_setup : MonoBehaviour {
-
-	public float height;
-	public float tilt;
-	public float viewing_angle;
+	
+	private const float tilt = 41.28734f;
+	private const float viewing_angle = 0f;
 
 	// Use this for initialization
 	void Start () {
+		transform.rotation = new Quaternion(0,viewing_angle,tilt,360);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Transform.position.x
+	}
 }
