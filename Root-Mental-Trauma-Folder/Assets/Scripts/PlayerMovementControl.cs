94547cbbe9a840fea619ceac69943d706989bea0
Benjamin gordon <bgordon@bppwd.com>
2014-05-30 00:51:27 -0400

    Began to edit player physics

---
+++ Root-Mental-Trauma-Folder/Assets/Scripts/PlayerMovementControl.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementControl : MonoBehaviour 
+{
+
+	public float moveSpeed;
+	public float turnSpeed;
+	public Vector3 COM;
+	public WheelCollider FrontLeftWheel;
+	public WheelCollider FrontRightWheel;
+	public WheelCollider BackLeftWheel;
+	public WheelCollider BackRightWheel;
+
+	void Start()
+	{
+		rigidbody.centerOfMass += COM;
+	}
+
+	void FixedUpdate() 
+	{
+		if (Input.GetAxis ("Horizontal") > 0) {
+			FrontLeftWheel.motorTorque = Input.GetAxis ("Horizontal") * moveSpeed * Time.deltaTime;
+			BackLeftWheel.motorTorque = Input.GetAxis ("Horizontal") * moveSpeed * Time.deltaTime;
+			FrontRightWheel.motorTorque = -Input.GetAxis ("Horizontal") * moveSpeed * Time.deltaTime;
+			BackRightWheel.motorTorque = -Input.GetAxis ("Horizontal") * moveSpeed * Time.deltaTime;
+		} 
+		else if (Input.GetAxis ("Horizontal") < 0) 
+		{
+			FrontLeftWheel.motorTorque = -Input.GetAxis ("Horizontal") * moveSpeed * Time.deltaTime;
+			BackLeftWheel.motorTorque = -Input.GetAxis ("Horizontal") * moveSpeed * Time.deltaTime;
+			FrontRightWheel.motorTorque = Input.GetAxis ("Horizontal") * moveSpeed * Time.deltaTime;
+			BackRightWheel.motorTorque = Input.GetAxis ("Horizontal") * moveSpeed * Time.deltaTime;
+		}
+
+		if (Input.GetAxis ("Vertical") != 0) 
+		{
+			FrontLeftWheel.motorTorque = Input.GetAxis ("Vertical") * moveSpeed * Time.deltaTime;
+			BackLeftWheel.motorTorque = Input.GetAxis ("Vertical") * moveSpeed * Time.deltaTime;
+			FrontRightWheel.motorTorque = Input.GetAxis ("Vertical") * moveSpeed * Time.deltaTime;
+			BackRightWheel.motorTorque = Input.GetAxis ("Vertical") * moveSpeed * Time.deltaTime;
+		}
+	}
+}
+
--- Root-Mental-Trauma-Folder/Assets/Scripts/pc_camera_setup.cs
+++ Root-Mental-Trauma-Folder/Assets/Scripts/pc_camera_setup.cs
@@ -2,16 +2,13 @@
 using System.Collections;
 
 public class pc_camera_setup : MonoBehaviour {
-	
-	private const float tilt = 41.28734f;
-	private const float viewing_angle = 0f;
 
 	// Use this for initialization
 	void Start () {
-		transform.rotation = new Quaternion(0,viewing_angle,tilt,360);
+
 	}
-	
 	// Update is called once per frame
 	void Update () {
+
 	}
 }
